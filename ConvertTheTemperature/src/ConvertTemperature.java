125e09997bb3546a39d198312b6e9fe3b2416ed2
Tiffany Obi <tiffanyobi123@gmail.com>
2024-01-09 14:09:21 -0500

    java/js challenges uploaded

---
+++ ConvertTheTemperature/src/ConvertTemperature.java
@@ -0,0 +1,64 @@
+public class ConvertTemperature {
+
+    /**
+     * LeetCode link: https://leetcode.com/problems/convert-the-temperature/description/
+     *
+     You are given a non-negative floating point number rounded to two decimal places Celsius, that denotes the temperature in Celsius.
+
+     You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, Fahrenheit].
+
+     Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+
+     Note that:
+
+     Kelvin = Celsius + 273.15
+     Fahrenheit = Celsius * 1.80 + 32.00
+
+
+     Example 1:
+
+     Input: Celsius = 36.50
+     Output: [309.65000,97.70000]
+     Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+
+     Example 2:
+
+     Input: Celsius = 122.11
+     Output: [395.26000,251.79800]
+     Explanation: Temperature at 122.11 Celsius converted in Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+
+
+     Constraints:
+
+     0 <= Celsius <= 1000
+     */
+
+
+        public static double[] convertTemperature(double celsius) {
+
+            //create the array that will be returned
+             ans = new double[2]
+
+            //create variable to hold kelvin value (should be a double)
+            //convert Celsius to kelvin
+            // add it to the array
+            char kelvin;
+
+            ans[] = kelvin
+
+
+            //create variable to hold Fahrenheit value (should be a double)
+            //convert Celsius to Fahrenheit
+            // add it to the array
+            int fahrenheit;
+
+            ans[] = fahrenheit
+
+
+            //return the ans array
+            return ans
+
+
+        }
+
+}
---
+++ JSConvertTheTemperature/ConvertTemperature.js
@@ -0,0 +1,64 @@
+/**
+ * LeetCode link: https://leetcode.com/problems/convert-the-temperature/description/
+ *
+ You are given a non-negative floating point number rounded to two decimal places Celsius, that denotes the temperature in Celsius.
+
+ You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, Fahrenheit].
+
+ Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+
+ Note that:
+
+ Kelvin = Celsius + 273.15
+ Fahrenheit = Celsius * 1.80 + 32.00
+
+
+ Example 1:
+
+ Input: Celsius = 36.50
+ Output: [309.65000,97.70000]
+ Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+
+ Example 2:
+
+ Input: Celsius = 122.11
+ Output: [395.26000,251.79800]
+ Explanation: Temperature at 122.11 Celsius converted in Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+
+
+ Constraints:
+
+ 0 <= Celsius <= 1000
+ */
+function convertTemperature(celsius){
+
+     //create the array that will be returned
+        let ans
+
+    //create variable to hold kelvin value
+        //convert Celsius to kelvin
+        // add it to the array
+        let kelvin;
+        
+        ans.push();
+
+
+
+        //create variable to hold Fahrenheit value
+        //convert Celsius to Fahrenheit
+        // add it to the array
+       
+        fahrenheit = (1.80);
+        
+
+
+        //return the ans array
+        return ans;
+
+ }
+
+ console.log(convertTemperature(150));
+
+ // to run javascript you must first be in the correct folder
+ //then you will type  " node 'nameOfFile.js' " in the console
+ //Example: node ConvertTemperature.js
